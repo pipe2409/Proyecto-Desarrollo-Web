bed665816a9a0909faee83bb2d45b08b5e7060f2
pipe2409 <37087427+pipe2409@users.noreply.github.com>
2026-03-01 12:58:15 -0500

    cambios menores

--- demo/src/main/java/com/example/demo/controller/HuespedesController.java
+++ demo/src/main/java/com/example/demo/controller/HuespedesController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.repository.HuespedesRepositoryRepository;
+import com.example.demo.repository.HuespedesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
