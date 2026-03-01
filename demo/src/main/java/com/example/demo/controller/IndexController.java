8871dfb433142ec479a48a0e4bff8666c7b4384a
pipe2409 <37087427+pipe2409@users.noreply.github.com>
2026-03-01 13:47:43 -0500

    Pagina iniciar sesión
    Falta mejorar la posición de la navbar

--- demo/src/main/java/com/example/demo/controller/IndexController.java
+++ demo/src/main/java/com/example/demo/controller/IndexController.java
@@ -3,15 +3,21 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
+@RequestMapping("")
 @Controller
 public class IndexController {
 
-
-    @GetMapping("/index")
+    @GetMapping("")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/iniciar-sesion")
+    public String iniciarSesion() {
+        return "iniciar-sesion";
     }
 
 }
