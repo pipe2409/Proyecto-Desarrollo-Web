81bbedcd4407494cca417a4c26e0a3ae54dcaaa0
pipe2409 <andresbe1205@gmail.com>
2026-03-16 21:37:37 -0500

    Cambiar ruta btn reservar en pag perfil

--- demo/src/main/java/com/example/demo/repository/ReservaRepository.java
+++ demo/src/main/java/com/example/demo/repository/ReservaRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.entities.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
